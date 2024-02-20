d92321a16f06fb9ffe38ab6956179f7f9df58358
rancarras <160468053+rancarras@users.noreply.github.com>
2024-02-20 15:52:41 +0100

    Added original (buggy) dicegame files

---
+++ dice_game/main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 20 15:12:33 2024
+
+@author: ranca
+"""
+
+from dicegame.runner import GameRunner
+
+
+def main():
+    print("Add the values of the dice")
+    print("It's really that easy")
+    print("What are you doing with your life.")
+    breakpoint()
+    GameRunner.run()
+
+
+if __name__ == "__main__":
+    main()
